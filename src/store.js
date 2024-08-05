5fe8cc045c4b9bfa7ea8f24b5b6335afcf529640
Whalley7 <hallettwendy@gail.com>
2024-08-05 15:13:31 -0400

    update store commit

--- src/store.js
+++ src/store.js
@@ -1,9 +1,18 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+//add avSlice from ./avSlcie
+import avReducer from './avSlice';
+//add mealSlice from ./mealSlice
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer
   },
 });
+
+
+
+
+
