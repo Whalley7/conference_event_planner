e9f197033a50e0c3d2a54fc567756152ab5817db
Whalley7 <hallettwendy@gmail.com>
2024-08-07 21:47:24 +0000

    "change store js"

--- src/store.js
+++ src/store.js
@@ -1,22 +1,13 @@
+// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-// added adReducer and mealsRedcer
 import avReducer from './avSlice';
 import mealsReducer from './mealsSlice';
 
-// Configure the Redux store
-const store = configureStore({
+export default configureStore({
   reducer: {
     venue: venueReducer,
     av: avReducer,
     meals: mealsReducer,
   },
-  //added middleware to the store.js
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-  devTools: process.env.NODE_ENV !== 'production',
 });
-
-export default store;
