9708a72eeb66da37f79ec8a0108700f18fd86e32
Whalley7 <hallettwendy@gmail.com>
2024-08-07 20:52:44 +0000

    "script change"

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4173, // Replace 4173 with your desired port number
-  },
+  
 })
 
