fb135bc5a0a61596de590f2752623c2caeb36fa9
Whalley7 <hallettwendy@gmail.com>
2025-04-20 11:33:27 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/conference_event_planner/',
+
+
   server: {
     host: true,
     allowedHosts: ['.gitpod.io'] // ✅ allow Gitpod
