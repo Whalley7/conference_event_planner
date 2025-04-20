848222ab6b3538e30798f5d328a2dc9f7b1b4da0
Whalley7 <hallettwendy@gmail.com>
2025-04-20 09:41:11 +0000

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
-
-
-
-
+  base: '/conference_event_planner/', // 👈 Add this line
+});
